6b849cf08c935bfce42b33139587b7951540e19b
Søren Dyhr <siadyhr@gmail.com>
2015-02-22 21:11:59 +0100

    Skip text animation
    Signed-off-by: Søren Dyhr <siadyhr@gmail.com>

--- YAPLGE.lua
+++ YAPLGE.lua
@@ -6,12 +6,19 @@ local time=0
 function YAPLGE.draw.notify(text,duration,dt,key)
 	time = time+dt
 	stringLength=stringLength or 0
-	localtext=localtext
+--	localtext=localtext
 	if time > stringLength * duration/string.len(text) then
 		stringLength = stringLength + 1
-		localtext = string.sub(text,1,stringLength)
 	end
+	localtext = string.sub(text,1,stringLength)
 	love.graphics.print(localtext,love.graphics.getWidth()/2,love.graphics.getHeight()-32)
+	if love.keyboard.isDown(key) then
+		if stringLength < string.len(text) then
+			stringLength = string.len(text)
+		elseif stringLenth == string.len(text) then
+			YAPLGE.draw.notifyClear()
+		end
+	end
 end
 
 function YAPLGE.draw.notifyClear()
