af231c6699f7b5bfbd3c74202b689771042da432
Søren Dyhr <siadyhr@gmail.com>
2015-02-22 19:11:51 +0100

    Simplify declarations and add notifyClear()
    Signed-off-by: Søren Dyhr <siadyhr@gmail.com>

--- YAPLGE.lua
+++ YAPLGE.lua
@@ -1,16 +1,21 @@
 local YAPLGE = {
 	draw={}
 }
+local time=0
 
 function YAPLGE.draw.notify(text,duration,dt,key)
-	time = if time then time+dt else 0 end
+	time = time+dt
 	stringLength=stringLength or 0
-	localtext=localtext or ""
+	localtext=localtext
 	if time > stringLength * duration/string.len(text) then
 		stringLength = stringLength + 1
 		localtext = string.sub(text,1,stringLength)
 	end
 	love.graphics.print(localtext,love.graphics.getWidth()/2,love.graphics.getHeight()-32)
+end
+
+function YAPLGE.draw.notifyClear()
+	time,stringLength,localtext=0,0,""
 end
 
 return YAPLGE
