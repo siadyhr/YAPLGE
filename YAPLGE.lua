36c2bdaba025195b135270041c1664022803eb06
Søren Dyhr <siadyhr@gmail.com>
2015-02-22 01:27:47 +0100

    Beginning of notify function
    Signed-off-by: Søren Dyhr <siadyhr@gmail.com>

---
+++ YAPLGE.lua
@@ -0,0 +1,18 @@
+local YAPLGE = {}
+
+function YAPLGE.notify(text,duration,dt,key)
+	if time then
+		time=time+dt
+	else
+		time=0
+	end
+	stringLength=stringLength or 0
+	localtext=localtext or ""
+	if time > stringLength * duration/string.len(text) then
+		stringLength = stringLength + 1
+		localtext = string.sub(text,1,stringLength)
+	end
+	love.graphics.print(localtext,love.graphics.getWidth()/2,love.graphics.getHeight()-32)
+end
+
+return YAPLGE
