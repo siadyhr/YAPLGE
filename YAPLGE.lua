6178e9cad47687b447aa93d044651d75e3e11dca
Søren Dyhr <siadyhr@gmail.com>
2015-05-18 22:02:58 +0200

    Add import 2d tilemaps
    Signed-off-by: Søren Dyhr <siadyhr@gmail.com>

--- YAPLGE.lua
+++ YAPLGE.lua
@@ -3,6 +3,22 @@ local YAPLGE = {
 }
 local time=0
 
+function YAPLGE.importTileMap(file,tileSize)
+	local tileMap = love.graphics.newImage(file)
+	local quads = {}
+	quadsH = tileMap:getWidth()/tileSize	-- Number of horisontal quads
+	quadsV = tileMap:getHeight()/tileSize	-- Number of vertical quads
+
+	for i = 1, quadsV do
+		for j = 1, quadsH do
+			print((i*quadsH)-quadsH + j)
+			quads[(i*quadsH)-quadsH + j] = love.graphics.newQuad((i-1)*tileSize , (j-1)*tileSize , tileSize , tileSize , tileMap:getWidth() , tileMap:getWidth())
+		end
+	end
+	
+	return quads
+end
+
 function YAPLGE.draw.notify(text,duration,dt,key)
 	time = time+dt
 	stringLength=stringLength or 0
