7bdcdd72cd5780d19fb86c49bb742bdf27a10555
Søren Dyhr <siadyhr@gmail.com>
2015-05-27 09:33:00 +0200

    Make changing maps work better by subtracting the right numbers
    Signed-off-by: Søren Dyhr <siadyhr@gmail.com>

--- main.lua
+++ main.lua
@@ -7,22 +7,26 @@ function love.load()
 		x=0,
 		y=0,
 		speed=100,
-		image = love.graphics.newImage('player.png')
+		image = love.graphics.newImage('player.png'),
 	}
+	player.width,player.height = player.image:getDimensions()
 	tileSize=32
-	scale=4
+	scale=2
+	mapWidth, mapHeight = 0,0
 
 translation = { "#" , " " , "+", "o", "e", "f", "i", "h"}
 	quadInfo = YAPLGE.importTileMap('tilemap.png' ,tileSize, translation)
 	maps.row, maps.column = 1,1
 	tileMapTable = YAPLGE.stringToTable(maps.strings[maps.strings[maps.row][maps.column]])
-	print(maps.strings[1][1])
 	tileMap = love.graphics.newImage('tilemap.png')
 end
 
 function love.update(dt)
 	deltatime=dt
 	atime=atime+dt
+	mapWidth = scale * #tileMapTable[1]*tileSize
+	mapHeight = scale * #tileMapTable * tileSize
+
 	if love.keyboard.isDown("up") then
 		player.y = player.y - player.speed*dt
 	end
@@ -35,20 +39,33 @@ function love.update(dt)
 	if love.keyboard.isDown("left") then
 		player.x = player.x - player.speed*dt
 	end
---	print(YAPLGE.getTile(player.x+player.image:getWidth(), player.y+player.image:getHeight(), 16, tileMapTable, scale))
 
-	print(maps.row)
-
-	if player.y > scale*tileSize then -- Why don't I have to take number of tiles into account?
+	if player.y + player.height > mapHeight then --TODO: Do something better than 128 by getting width of table or something
 		maps.row = maps.row + 1
-		tileMapTable = YAPLGE.stringToTable(maps.strings[maps.strings[maps.row][maps.column]])
 		player.y = 0
+		tileMapTable = YAPLGE.stringToTable(maps.strings[maps.strings[maps.row][maps.column]])
 	end
 	if player.y < 0 then
-		maps.row = maps.row - 1
-		player.y = 128 --Generalize
+		if maps.row ~= 1 then
+			maps.row = maps.row - 1
+			player.y = 128 --Generalize
+			tileMapTable = YAPLGE.stringToTable(maps.strings[maps.strings[maps.row][maps.column]])
+		end
+	end
+	if player.x + player.width > mapWidth then
+		maps.column = maps.column + 1
+		player.x = 0
 		tileMapTable = YAPLGE.stringToTable(maps.strings[maps.strings[maps.row][maps.column]])
 	end
+	if player.x < 0 then
+		if maps.column ~= 1 then 
+			maps.column = maps.column - 1
+			player.x = 128 --Generalize
+			tileMapTable = YAPLGE.stringToTable(maps.strings[maps.strings[maps.row][maps.column]])
+		end
+	end
+
+	print(YAPLGE.getTile(player.x+player.width, player.y+player.height, tileSize, tileMapTable, scale))
 end
 
 function love.draw()
