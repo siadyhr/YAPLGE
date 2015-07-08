e4be45c830939d0da38949acb383f39ecd579259
Søren Dyhr <siadyhr@gmail.com>
2015-07-08 23:38:10 +0200

    Now with non-square tiles
    Signed-off-by: Søren Dyhr <siadyhr@gmail.com>

--- import.lua
+++ import.lua
@@ -2,7 +2,7 @@ local import = {
 }
 local time=0
 
-function import.TileMap(file,tileSize,translation)
+function import.TileMap(file,tileSizeX,tileSizeY,translation)
 	--[[
 	Converts an image file with a translation table
 	into a table containing { translation, x, y}
@@ -11,8 +11,8 @@ function import.TileMap(file,tileSize,translation)
 	local quads = {}
 	local realQuadInfo   = {}
 	local width, height = tileMap:getDimensions()
-	quadsH = width/tileSize		-- Number of horisontal quads
-	quadsV = height/tileSize	-- Number of vertical quads
+	quadsH = width/tileSizeX		-- Number of horisontal quads
+	quadsV = height/tileSizeY	-- Number of vertical quads
 	for i = 1, quadsV*quadsH do
 		quads[i] = {}
 	end
@@ -20,12 +20,12 @@ function import.TileMap(file,tileSize,translation)
 	for i = 1, quadsV do
 		for j = 1, quadsH do
 			local number = (i*quadsH)-quadsH+j
-			quads[number] = { translation[number] , (j-1)*tileSize , (i-1)*tileSize}
+			quads[number] = { translation[number] , (j-1)*tileSizeX , (i-1)*tileSizeY}
 		end
 	end
 
 	for _,quadInfo in ipairs(quads) do
-		realQuadInfo[quadInfo[1]] = love.graphics.newQuad(quadInfo[2] ,quadInfo[3], tileSize, tileSize, width, height)
+		realQuadInfo[quadInfo[1]] = love.graphics.newQuad(quadInfo[2] ,quadInfo[3], tileSizeX, tileSizeY, width, height)
 	end
 
 	return realQuadInfo
